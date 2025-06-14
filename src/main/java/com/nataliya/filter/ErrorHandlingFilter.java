b8f016bc3857ecc38c85e99618e8564943e55a2d
Nataliya <n.basanina@gmail.com>
2025-06-14 23:25:06 +0300

    refactor: ErrorHandlingFilter 'implements Filter' change to 'extends HttpFilter'

--- src/main/java/com/nataliya/filter/ErrorHandlingFilter.java
+++ src/main/java/com/nataliya/filter/ErrorHandlingFilter.java
@@ -4,6 +4,7 @@ import com.nataliya.exception.CustomException;
 import com.nataliya.util.JspUtil;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -12,17 +13,15 @@ import java.io.IOException;
 
 @WebFilter("/*")
 @Slf4j
-public class ErrorHandlingFilter implements Filter {
+public class ErrorHandlingFilter extends HttpFilter {
 
     private static final String ERROR_JSP_NAME = "error";
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(HttpServletRequest req, HttpServletResponse resp, FilterChain filterChain) throws IOException, ServletException {
 
-        HttpServletRequest req = (HttpServletRequest) servletRequest;
-        HttpServletResponse resp = (HttpServletResponse) servletResponse;
         try {
-            filterChain.doFilter(servletRequest, servletResponse);
+            filterChain.doFilter(req, resp);
         } catch (CustomException e) {
             logException(e);
             sendErrorResponse(e, req, resp);
@@ -30,7 +29,7 @@ public class ErrorHandlingFilter implements Filter {
             log.error(e.getMessage(), e);
             req.setAttribute("status", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             req.setAttribute("message", "Something is wrong: " + e.getMessage());
-            resp.setStatus( HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             req.getRequestDispatcher(JspUtil.getPath(ERROR_JSP_NAME)).forward(req, resp);
         }
     }
@@ -42,8 +41,8 @@ public class ErrorHandlingFilter implements Filter {
         req.getRequestDispatcher(JspUtil.getPath(ERROR_JSP_NAME)).forward(req, resp);
     }
 
-    private void logException(CustomException e){
-        if (e.getStatusCode() == HttpServletResponse.SC_BAD_REQUEST || e.getStatusCode() == HttpServletResponse.SC_NOT_FOUND){
+    private void logException(CustomException e) {
+        if (e.getStatusCode() == HttpServletResponse.SC_BAD_REQUEST || e.getStatusCode() == HttpServletResponse.SC_NOT_FOUND) {
             log.info(e.getMessage(), e);
         } else {
             log.warn(e.getMessage(), e);
